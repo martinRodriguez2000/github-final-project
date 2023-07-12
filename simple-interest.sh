de5481cbd5444cbf54352bc12af024b480bdd1d4
martinRodriguez2000 <139281301+martinRodriguez2000@users.noreply.github.com>
2023-07-12 19:23:36 -0300

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # <your GitHub username>
+   # martinRodriguez2000
    # Input:
    # p, principal amount
    # t, time period in years
@@ -20,3 +20,4 @@
    s=`expr $p \* $t \* $r / 100`
    echo "The simple interest is: "
    echo $s
+  
